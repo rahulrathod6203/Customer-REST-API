66e348c5455c5e8f45a837e466c669776628f7ac
Rahul Rathod <rahulrathod6203@gmail.com>
2023-06-13 15:43:08 +0530

    GetMapping -> Get All customers endpoint added

--- src/main/java/com/cg/controller/CustomerController.java
+++ src/main/java/com/cg/controller/CustomerController.java
@@ -1,8 +1,27 @@
 package com.cg.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cg.entity.Customer;
+import com.cg.service.CustomerServiceImpl;
+
 @RestController
+@RequestMapping("/api/v1")
 public class CustomerController {
+	
+	@Autowired
+	CustomerServiceImpl service;
+	
+	@GetMapping("/customer/all")
+	public List<Customer> getAllCustomers(){
+		return service.getAll();
+	}
+	
+	
 
 }
--- src/main/java/com/cg/service/CustomerServiceImpl.java
+++ src/main/java/com/cg/service/CustomerServiceImpl.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.cg.entity.Customer;
 import com.cg.repo.CustomerRepo;
 
+@Service
 public class CustomerServiceImpl implements CustomerSerivce {
 
 	@Autowired
