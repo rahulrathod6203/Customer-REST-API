1cc628ced58f587955708d18c4e9e060b26b163f
Rahul Rathod <rahulrathod6203@gmail.com>
2023-06-14 14:42:54 +0530

    PostMapping endpoint added

--- src/main/java/com/cg/controller/CustomerController.java
+++ src/main/java/com/cg/controller/CustomerController.java
@@ -6,12 +6,16 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.entity.Customer;
 import com.cg.exceptions.CustomerNotFoundException;
 import com.cg.service.CustomerServiceImpl;
+
+import jakarta.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -29,6 +33,12 @@ public class CustomerController {
 	public Optional<Customer> getById(@PathVariable("id") int id) {
 		return Optional.of(service.getById(id).orElseThrow(() -> new CustomerNotFoundException(id)));
 	}
+
+	@PostMapping("/customer/new")
+	public Customer addNewCustomer(@RequestBody Customer customer) {
+		return service.addNewCustomer(customer);
+	}
 	
 	
+
 }
